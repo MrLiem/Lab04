b672c345c6b040424e62b89c3d2a1b5a985ec098
MrLiem <liem12t@gmail.com>
2020-12-06 22:12:27 +0700

    fix split null

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,11 @@
 const { User } = require("../models/user");
 
 const auth = (req, res, next) => {
-  const token = req.headers.authorization.split(" ")[1];
-  console.log(token);
+  let token;
+  if (req.headers) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+  // console.log(token);
   User.findByToken(token, (err, user) => {
     if (err) throw err;
     if (!user) return res.json({ isAuth: false, error: true });
